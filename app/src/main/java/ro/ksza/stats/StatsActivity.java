5e79194bacf6e0319ced3ae1e7be59fb1b2f2a5f
ksza <karoly.szanto@3pillarglobal.com>
2015-12-05 16:19:24 +0200

    Added swipe to dismiss

--- app/src/main/java/ro/ksza/stats/StatsActivity.java
+++ app/src/main/java/ro/ksza/stats/StatsActivity.java
@@ -1,11 +1,15 @@
 package ro.ksza.stats;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
 import ro.ksza.stats.list.MoviesAdapter;
+import ro.ksza.stats.model.Person;
 import ro.ksza.stats.model.StatsDao;
 
 public class StatsActivity extends AppCompatActivity {
@@ -34,7 +38,27 @@ public class StatsActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+
+        setUpSwipeToDismiss();
     }
 
-    
+    private void setUpSwipeToDismiss() {
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+
+                final int position = viewHolder.getAdapterPosition();
+                final Person item = adapter.getItemAt(position);
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
 }
